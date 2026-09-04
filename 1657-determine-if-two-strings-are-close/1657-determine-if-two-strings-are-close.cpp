9f8cd2eaa6d1842ce3fbbcfaa704795414a350d2
Shivam Kumar <161005737+Shivam-1502@users.noreply.github.com>
2026-09-04 23:17:02 +0530

    Time: 33 ms (25.92%), Space: 124.7 MB (5.44%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -13,11 +13,19 @@ public:
             if((freq1[i] == 0) != (freq2[i] == 0)) return false;
         }
 
-        sort(freq1.begin(), freq1.end());
-        sort(freq2.begin(), freq2.end());
+        vector<int> freqOfFreq1(100001, 0);
+        vector<int> freqOfFreq2(100001, 0);
         
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) return false;
+        for (int f : freq1) {
+            if (f > 0) freqOfFreq1[f]++;
+        }
+        
+        for (int f : freq2) {
+            if (f > 0) freqOfFreq2[f]++;
+        }
+
+        for (int i = 1; i < freqOfFreq1.size(); i++) {
+            if (freqOfFreq1[i] != freqOfFreq2[i]) return false;
         }
         
         return true;
